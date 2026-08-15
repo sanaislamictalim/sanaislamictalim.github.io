0eb48571a6c7ebc68b548f35fb8bc97cc2150956
sanaislamictalim <sanaislamictalim@gmail.com>
2026-08-15 19:03:54 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAn3S_GQvt3Vd99JDzjfPEi5OuxDBasqaQ",
+  authDomain: "sanaislamictalim-3f36b.firebaseapp.com",
+  projectId: "sanaislamictalim-3f36b",
+  storageBucket: "sanaislamictalim-3f36b.firebasestorage.app",
+  messagingSenderId: "783399416662",
+  appId: "1:783399416662:web:cb52166ef262da565e9cf7"
+});
+
+const messaging = firebase.messaging();
 
