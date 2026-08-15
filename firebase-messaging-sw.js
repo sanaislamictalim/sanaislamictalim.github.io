f338092c5f34cd9e7d995f1e9f9d01215d6d96cc
sanaislamictalim <sanaislamictalim@gmail.com>
2026-08-15 20:03:02 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,121 @@
-importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js"
+);
+
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js"
+);
+
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAn3S_GQvt3Vd99JDzjfPEi5OuxDBasqaQ",
-  authDomain: "sanaislamictalim-3f36b.firebaseapp.com",
-  projectId: "sanaislamictalim-3f36b",
-  storageBucket: "sanaislamictalim-3f36b.firebasestorage.app",
-  messagingSenderId: "783399416662",
-  appId: "1:783399416662:web:cb52166ef262da565e9cf7"
+
+  apiKey:
+    "AIzaSyAn3S_GQvt3Vd99JDzjfPEi5OuxDBasqaQ",
+
+  authDomain:
+    "sanaislamictalim-3f36b.firebaseapp.com",
+
+  projectId:
+    "sanaislamictalim-3f36b",
+
+  storageBucket:
+    "sanaislamictalim-3f36b.firebasestorage.app",
+
+  messagingSenderId:
+    "783399416662",
+
+  appId:
+    "1:783399416662:web:cb52166ef262da565e9cf7"
+
 });
 
-const messaging = firebase.messaging();
 
+const messaging =
+  firebase.messaging();
+
+
+messaging.onBackgroundMessage(
+  function(payload){
+
+    console.log(
+      "Background message:",
+      payload
+    );
+
+    const notification =
+      payload.notification || {};
+
+    const title =
+      notification.title ||
+      "🕌 Sana Islamic";
+
+    const options = {
+
+      body:
+        notification.body ||
+        "नई Islamic notification आई है।",
+
+      icon:
+        "/assets/header_logo.png",
+
+      badge:
+        "/assets/header_logo.png"
+
+    };
+
+
+    self.registration.showNotification(
+      title,
+      options
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  "notificationclick",
+  function(event){
+
+    event.notification.close();
+
+    event.waitUntil(
+
+      clients.matchAll({
+        type: "window",
+        includeUncontrolled: true
+      }).then(function(clientList){
+
+        for(
+          const client of clientList
+        ){
+
+          if(
+            client.url ===
+              "https://sanaislamictalim.github.io/" &&
+            "focus" in client
+          ){
+
+            return client.focus();
+
+          }
+
+        }
+
+
+        if(
+          clients.openWindow
+        ){
+
+          return clients.openWindow(
+            "https://sanaislamictalim.github.io/"
+          );
+
+        }
+
+      })
+
+    );
+
+  }
+);
